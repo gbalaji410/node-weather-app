092716c08770bb1a8adc197c58c87431910d7343
Balaji Ganesan <balaji2104@gmail.com>
2020-01-28 10:48:36 -0800

    Heroku Changes

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,3 @@
-console.log('Client Side JavaScript is loaded.')
-
-fetch('http://puzzle.mead.io/puzzle').then((response) => {
-    response.json().then((data) => {
-        console.log(data)
-    })
-})
-
 const weatherForm = document.querySelector('form')
 const search = document.querySelector('input')
 const msg1 = document.querySelector('#msg1')
