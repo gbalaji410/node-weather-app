081235b901204a13a080c925de65840933d3d1a2
Balaji Ganesan <bganesan@walmartlabs.com>
2020-01-28 11:01:10 -0800

    Updating About Page

--- src/app.js
+++ src/app.js
@@ -30,7 +30,7 @@ app.get('', (req, res) => {
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Balaji'
+        name: 'Balaji Ganesan (GB)'
     })
 })
 
